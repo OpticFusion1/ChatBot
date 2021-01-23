bbf4eb64a623e26b634673b108a5934427a6c2e3
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2021-01-23 17:27:37 -0500

    Cleans up the code slightly

--- src/main/java/optic_fusion1/chatbot/bot/Bot.java
+++ src/main/java/optic_fusion1/chatbot/bot/Bot.java
@@ -21,8 +21,8 @@ import optic_fusion1.chatbot.bot.responses.CommandResponse;
 import optic_fusion1.chatbot.bot.translate.TranslateResponse;
 import optic_fusion1.chatbot.utils.FileUtils;
 import optic_fusion1.chatbot.utils.JSONUtils;
+import optic_fusion1.chatbot.utils.Utils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -104,8 +104,8 @@ public class Bot {
       ComponentBuilder componentBuilder = new ComponentBuilder();
       if (JSONUtils.isJSONValid(m)) {
         try {
-          componentBuilder.append(new TextComponent(ChatColor.translateAlternateColorCodes('&', prefix + " ")));
-          componentBuilder.append(ComponentSerializer.parse(ChatColor.translateAlternateColorCodes('&', translate(player, m, playerMessages))));
+          componentBuilder.append(new TextComponent(Utils.colorize(prefix + " ")));
+          componentBuilder.append(ComponentSerializer.parse(Utils.colorize(translate(player, m, playerMessages))));
         } catch (Exception e) {
           componentBuilder = new ComponentBuilder();
         }
@@ -114,10 +114,10 @@ public class Bot {
         String[] args = m.split("\n");
         for (String arg : args) {
           if (silent) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', translate(player, prefix + " " + arg, playerMessages)));
+            player.sendMessage(Utils.colorize(translate(player, prefix + " " + arg, playerMessages)));
             continue;
           }
-          Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', translate(player, prefix + " " + arg, playerMessages)));
+          Bukkit.broadcastMessage(Utils.colorize(translate(player, prefix + " " + arg, playerMessages)));
         }
         return;
       }
@@ -126,14 +126,14 @@ public class Bot {
           player.spigot().sendMessage(componentBuilder.create());
           return;
         }
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', translate(player, prefix + " " + m, playerMessages)));
+        player.sendMessage(Utils.colorize(translate(player, prefix + " " + m, playerMessages)));
         return;
       }
       if (!componentBuilder.getParts().isEmpty()) {
         Bukkit.spigot().broadcast(componentBuilder.create());
         return;
       }
-      Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', translate(player, prefix + " " + m, playerMessages)));
+      Bukkit.broadcastMessage(Utils.colorize(translate(player, prefix + " " + m, playerMessages)));
     }, responseSpeed);
   }
 
